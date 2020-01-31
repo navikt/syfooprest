fe7e250f92f50d26864ba0f94b40ba5288b1adc4
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-01-31 09:05:49 +0100

    Remove unused endopint for personRessurs with virksomhetsnummer

--- src/main/java/no/nav/syfo/services/TilgangskontrollService.java
+++ src/main/java/no/nav/syfo/services/TilgangskontrollService.java
@@ -1,33 +1,20 @@
 package no.nav.syfo.services;
 
-import no.nav.syfo.narmesteleder.Naermesteleder;
-import no.nav.syfo.narmesteleder.NarmesteLederConsumer;
 import no.nav.syfo.tilgang.BrukerTilgangConsumer;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
-import java.util.Optional;
 
 @Service
 public class TilgangskontrollService {
 
-    private final AktoerService aktoerService;
     private final BrukerTilgangConsumer brukerTilgangConsumer;
-    private final NarmesteLederConsumer narmesteLederConsumer;
 
     @Inject
     public TilgangskontrollService(
-            AktoerService aktoerService,
-            BrukerTilgangConsumer brukerTilgangConsumer,
-            NarmesteLederConsumer narmesteLederConsumer
+            BrukerTilgangConsumer brukerTilgangConsumer
     ) {
-        this.aktoerService = aktoerService;
         this.brukerTilgangConsumer = brukerTilgangConsumer;
-        this.narmesteLederConsumer = narmesteLederConsumer;
-    }
-
-    public boolean sporOmNoenAndreEnnSegSelvEllerEgneAnsatteEllerLedere(String innloggetFnr, String oppslattFnr, String virksomhetsnummer) {
-        return !(sporInnloggetBrukerOmSegSelv(innloggetFnr, oppslattFnr) || sporInnloggetBrukerOmEnAnsatt(oppslattFnr) || sporInnloggetBrukerOmEnLeder(innloggetFnr, oppslattFnr, virksomhetsnummer));
     }
 
     public boolean sporOmNoenAndreEnnSegSelvEllerEgneAnsatte(String innloggetFnr, String oppslattFnr) {
@@ -36,17 +23,6 @@ public class TilgangskontrollService {
 
     private boolean sporInnloggetBrukerOmEnAnsatt(String oppslattFnr) {
         return brukerTilgangConsumer.hasAccessToAnsatt(oppslattFnr);
-    }
-
-    private boolean sporInnloggetBrukerOmEnLeder(String innloggetFnr, String oppslattFnr, String virksomhetsnummer) {
-        Optional<Naermesteleder> lederForInnloggetBruker = narmesteLederConsumer.narmesteLeder(innloggetFnr, virksomhetsnummer);
-
-        if (lederForInnloggetBruker.isPresent()) {
-            String lederForInnloggetBrukerFnr = aktoerService.hentFnrForAktoer(lederForInnloggetBruker.get().naermesteLederAktoerId);
-            return oppslattFnr.equals(lederForInnloggetBrukerFnr);
-        } else {
-            return false;
-        }
     }
 
     private boolean sporInnloggetBrukerOmSegSelv(String innloggetFnr, String fnr) {
