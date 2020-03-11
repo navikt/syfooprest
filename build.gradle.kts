aa96c7d45e1c85be23562f8c0345bf04f9cfae19
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-03-11 09:41:48 +0100

    Revert "Reorganize list of dependencies"
    This reverts commit b745d72c16edae3fb3fb48f553402ed475367dfc.

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,24 +33,19 @@ repositories {
 }
 
 dependencies {
+    implementation("no.nav.tjenestespesifikasjoner:nav-arbeidsforhold-v3-tjenestespesifikasjon:1.2019.03.05-14.13-d95264192bc7")
+    implementation("no.nav.sbl:brukerprofil-v3-tjenestespesifikasjon:3.0.3")
+    implementation("no.nav.syfo.tjenester:aktoer-v2:1.0")
+    implementation("no.nav.syfo.tjenester:sykefravaersoppfoelgingv1-tjenestespesifikasjon:1.0.20")
+    implementation("no.nav.syfo.tjenester:dkif-tjenestespesifikasjon:1.2")
+    implementation("no.nav.sbl.dialogarena:organisasjonv4-tjenestespesifikasjon:1.0.1")
+
+    implementation("no.nav.tjenestespesifikasjoner:nav-fim-aktoer-v2-tjenestespesifikasjon:1.2019.07.10-12.21-b55f47790a9d")
+
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-jersey")
     implementation("org.springframework.boot:spring-boot-starter-logging")
-
-    implementation("no.nav.tjenestespesifikasjoner:nav-fim-aktoer-v2-tjenestespesifikasjon:1.2019.07.10-12.21-b55f47790a9d")
-    implementation("no.nav.tjenestespesifikasjoner:nav-arbeidsforhold-v3-tjenestespesifikasjon:1.2019.03.05-14.13-d95264192bc7")
-    implementation("no.nav.syfo.tjenester:aktoer-v2:1.0")
-    implementation("no.nav.syfo.tjenester:sykefravaersoppfoelgingv1-tjenestespesifikasjon:1.0.20")
-    implementation("no.nav.syfo.tjenester:dkif-tjenestespesifikasjon:1.2")
-    implementation("no.nav.sbl:brukerprofil-v3-tjenestespesifikasjon:3.0.3")
-    implementation("no.nav.sbl.dialogarena:organisasjonv4-tjenestespesifikasjon:1.0.1")
-
-    implementation("org.apache.cxf:cxf-rt-features-logging:$cxfVersion")
-    implementation("org.apache.cxf:cxf-rt-ws-security:$cxfVersion")
-    implementation("org.apache.cxf:cxf-rt-ws-policy:$cxfVersion")
-    implementation("org.apache.cxf:cxf-rt-transports-http:$cxfVersion")
-    implementation("org.apache.cxf:cxf-rt-frontend-jaxws:$cxfVersion")
 
     implementation("net.logstash.logback:logstash-logback-encoder:4.10")
     implementation("org.slf4j:slf4j-api:1.7.25")
@@ -61,9 +56,14 @@ dependencies {
     implementation("org.apache.commons:commons-lang3:3.5")
     implementation("net.sf.ehcache:ehcache:2.10.6")
     implementation("io.micrometer:micrometer-registry-prometheus:1.0.6")
-
     implementation("no.nav.security:oidc-support:$oidcSupportVersion")
     implementation("no.nav.security:oidc-spring-support:$oidcSupportVersion")
+
+    implementation("org.apache.cxf:cxf-rt-features-logging:$cxfVersion")
+    implementation("org.apache.cxf:cxf-rt-ws-security:$cxfVersion")
+    implementation("org.apache.cxf:cxf-rt-ws-policy:$cxfVersion")
+    implementation("org.apache.cxf:cxf-rt-transports-http:$cxfVersion")
+    implementation("org.apache.cxf:cxf-rt-frontend-jaxws:$cxfVersion")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("no.nav.security:oidc-spring-test:$oidcSupportTestVersion")
