e947bd0cbecb458d8e490d218224a1e6e624cc6f
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-07-08 12:46:13 +0200

    Remove unused evaluering from RSNaermesteleder

--- src/main/kotlin/no/nav/syfo/narmesteleder/controller/RSNaermesteLeder.kt
+++ src/main/kotlin/no/nav/syfo/narmesteleder/controller/RSNaermesteLeder.kt
@@ -1,19 +1,17 @@
 package no.nav.syfo.narmesteleder.controller
 
-import no.nav.syfo.rest.domain.RSEvaluering
 import java.time.LocalDate
 import java.time.LocalDateTime
 
 data class RSNaermesteLeder(
-        val virksomhetsnummer: String,
-        val erAktiv: Boolean,
-        val aktivFom: LocalDate,
-        val aktivTom: LocalDate?,
-        val navn: String = " ",
-        val fnr: String,
-        val epost: String?,
-        val tlf: String?,
-        val sistInnlogget: LocalDateTime?,
-        val samtykke: Boolean?,
-        val evaluering: RSEvaluering?
+    val virksomhetsnummer: String,
+    val erAktiv: Boolean,
+    val aktivFom: LocalDate,
+    val aktivTom: LocalDate?,
+    val navn: String = " ",
+    val fnr: String,
+    val epost: String?,
+    val tlf: String?,
+    val sistInnlogget: LocalDateTime?,
+    val samtykke: Boolean?
 )
