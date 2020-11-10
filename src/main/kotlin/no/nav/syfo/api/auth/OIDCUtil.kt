d626a45c1840592faf1e0a776c2dd5fae42318fd
Tor-Christian Tangenes <torchrct@gmail.com>
2020-11-10 13:21:45 +0100

    Refaktorer OIDCUtil.kt

--- src/main/kotlin/no/nav/syfo/api/auth/OIDCUtil.kt
+++ src/main/kotlin/no/nav/syfo/api/auth/OIDCUtil.kt
@@ -5,14 +5,14 @@ import no.nav.security.token.support.core.context.TokenValidationContextHolder
 
 object OIDCUtil {
 
-    @JvmStatic
     fun getSubjectEkstern(contextHolder: TokenValidationContextHolder): String {
+        val issuer = OIDCIssuer.EKSTERN
         val context: TokenValidationContext = contextHolder.tokenValidationContext
-        return context.getClaims(OIDCIssuer.EKSTERN)?.subject ?: throw RuntimeException("Fant ikke subject for OIDCIssuer Ekstern")
+        return context.getClaims(issuer)?.subject ?: throw RuntimeException("Fant ikke 'subject'-claim i token fra issuer: $issuer")
     }
 
     fun getIssuerToken(contextHolder: TokenValidationContextHolder, issuer: String): String {
         val context = contextHolder.tokenValidationContext
-        return context.getJwtToken(issuer)?.tokenAsString ?: throw RuntimeException("Klarte ikke hente token for issuer: $issuer")
+        return context.getJwtToken(issuer)?.tokenAsString ?: throw RuntimeException("Klarte ikke hente token fra issuer: $issuer")
     }
 }
